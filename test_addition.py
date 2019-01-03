2b2ee22dc238078da7f74a874212dd483baf67a8
ashley <ashley@example.com>
2019-01-03 15:30:54 +0000

    added subtract pytest

--- test_addition.py
+++ test_addition.py
@@ -1,5 +1,8 @@
 import pytest
-from addition import addition
+from addition import addition, subtract
 
 def test_add_a_to_b():
     assert addition(2,2) == 4
+
+def test_subtract_a_from_b():
+    assert subtract(3,4) == 1
